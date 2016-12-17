1631d300e6a3823775b1839e4f47512924e595a5
Laura Sempere <lrsempere@gmail.com>
2016-12-17 20:24:57 +0800

    Basic Tank Animation

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	public float moveForce = 3f;
+	public float maxSpeed = 5f;
+	public float velocity = 2f;
+
+	private float hSpeed = 0f;
+	private Animator anim;
+	private Transform pivot;
+	private Rigidbody2D rigidBody;
+	private float tilt;
+
+	void Awake () {
+		anim = GetComponent<Animator> ();
+		pivot = transform.FindChild ("Pivot");
+		rigidBody = GetComponent<Rigidbody2D> ();
+	}
+
+	void FixedUpdate () {
+		hSpeed = Input.GetAxis ("Horizontal");
+		anim.SetFloat ("Speed", Mathf.Abs(hSpeed));
+		rigidBody.velocity = new Vector2 (hSpeed * velocity, rigidBody.velocity.y);	
+
+		if (Input.GetKey(KeyCode.Z)){
+			tilt += 1.0f;
+		}
+
+		if (Input.GetKey(KeyCode.X)){
+			tilt -= 1.0f;
+		}
+
+		tilt = Mathf.Clamp (tilt, 0, 180);
+		pivot.rotation = Quaternion.Euler (0, 0, tilt);
+	}
+}
